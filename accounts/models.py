a9a6cf871b9180c3d3b5f3ee92fa282cdacbe30e
Alejoespino <alejoespinolascano@gmail.com>
2024-01-18 15:35:33 -0300

    creacion de orders

--- accounts/models.py
+++ accounts/models.py
@@ -4,44 +4,44 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
 
 class MyAccountManager(BaseUserManager):
-   def create_superuser(self, first_name, last_name, email, username,password=None): 
-    if not email:
-        raise ValueError('el usuario debe tener un email')
-    
-    if not username:
-        raise ValueError('el usuario debe tener un username')
-    
-    user = self.model(
-        email = self.normalize_email(email),
-        username = username,
-        first_name = first_name,
-        last_name = last_name,
-        is_admin = True,
-        is_superadmin = True,
-        is_staff = True,
-        is_active = True
-    )
+    def create_user(self, first_name, last_name, email, username,password=None): 
+        if not email:
+            raise ValueError('el usuario debe tener un email')
+        
+        if not username:
+            raise ValueError('el usuario debe tener un username')
+        
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+            is_admin = True,
+            is_superadmin = True,
+            is_staff = True,
+            is_active = True
+        )
 
-    user.set_password(password)
-    user.save(using=self._db)
-    return user
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-def create_superuser(self, first_name,last_name, email , username,password ):
-    user = self.create_user(
-        email = self.normalize_email(email),
-        username = username,
-        password = password,
-        first_name = first_name,
-        last_name = last_name,
+    def create_superuser(self, first_name,last_name, email , username,password ):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
 
-    )
+        )
 
-    user.is_admin = True
-    user.is_activate = True
-    user.is_staff = True
-    user.is_superadmin = True
-    user.save(using=self._db)
-    return user
+        user.is_admin = True
+        user.is_activate = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
 
 
 class Account(AbstractBaseUser):
