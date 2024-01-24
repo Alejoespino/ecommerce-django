6fbeb1f669a8799521a54285b31de6e88ed5d15e
Alejoespino <alejoespinolascano@gmail.com>
2024-01-24 12:29:33 -0300

    update code

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY='django-insecure-=-cy(a#uesgnfo83sx2ad34#a((6mh_0v0^=82(*4(va8!sc_h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool, default=True)
+DEBUG = True
 
-ALLOWED_HOSTS = ['ecommerce-env.eba-nmr2hdmp.us-west-2.elasticbeanstalk.com/']
+ALLOWED_HOSTS = ['ecommerce-env.eba-nmr2hdmp.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
